f8dfae7493055d82bbd50345152e53eee3754570
Doug Binks <doug@enkisoftware.com>
2017-09-08 17:03:57 +0100

    Added OS X semaphores and moved all semaphores to own section of file.

--- src/Threads.h
+++ src/Threads.h
@@ -34,10 +34,6 @@
 namespace enki
 {
     typedef HANDLE threadid_t;
-    struct semaphoreid_t
-    {
-        HANDLE      sem;
-    };
 
     // declare the thread start function as:
     // THREADFUNC_DECL MyThreadStart( void* pArg );
@@ -61,8 +57,55 @@ namespace enki
         GetSystemInfo(&sysInfo);
         return sysInfo.dwNumberOfProcessors;
     }
+}
 
-    inline void SemaphoreCreate( semaphoreid_t& semaphoreid )
+#else // posix
+
+	#include <pthread.h>
+	#include <unistd.h>
+	#define THREADFUNC_DECL void*
+	#define THREAD_LOCAL __thread
+
+namespace enki
+{
+    typedef pthread_t threadid_t;  
+        
+    // declare the thread start function as:
+    // THREADFUNC_DECL MyThreadStart( void* pArg );
+    inline bool ThreadCreate( threadid_t* returnid, void* ( *StartFunc) (void* ), void* pArg )
+    {
+        // posix equiv pthread_create
+        int32_t retval = pthread_create( returnid, NULL, StartFunc, pArg );
+
+        return  retval == 0;
+    }
+    
+    inline bool ThreadTerminate( threadid_t threadid )
+    {
+        // posix equiv pthread_cancel
+        return pthread_cancel( threadid ) == 0;
+    }
+    
+    inline uint32_t GetNumHardwareThreads()
+    {
+        return (uint32_t)sysconf( _SC_NPROCESSORS_ONLN );
+    }
+}
+
+#endif // posix
+
+
+// Semaphore implementation
+#ifdef _WIN32
+
+namespace enki
+{
+    struct semaphoreid_t
+    {
+        HANDLE      sem;
+    };
+	
+	inline void SemaphoreCreate( semaphoreid_t& semaphoreid )
     {
         semaphoreid.sem = CreateSemaphore(NULL, 0, MAXLONG, NULL );
     }
@@ -87,46 +130,57 @@ namespace enki
 		}
     }
 }
+#elif defined(__MACH__)
 
-#else // posix
-
-	#include <pthread.h>
-	#include <semaphore.h>
-	#include <unistd.h>
-	#define THREADFUNC_DECL void*
-	#define THREAD_LOCAL __thread
+// OS X does not have POSIX semaphores
+// see https://developer.apple.com/library/content/documentation/Darwin/Conceptual/KernelProgramming/synchronization/synchronization.html
+#include <mach/mach.h>
 
 namespace enki
 {
-    typedef pthread_t threadid_t;
+    
+    struct semaphoreid_t
+    {
+        semaphore_t   sem;
+    };
+	
+	inline void SemaphoreCreate( semaphoreid_t& semaphoreid )
+    {
+		semaphore_create( mach_task_self(), &semaphoreid.sem, SYNC_POLICY_FIFO, 0 );
+    }
+    
+    inline void SemaphoreClose( semaphoreid_t& semaphoreid )
+    {
+        semaphore_destroy( mach_task_self(), semaphoreid.sem );
+    }
+    
+    inline void SemaphoreWait( semaphoreid_t& semaphoreid  )
+    {
+        semaphore_wait( semaphoreid.sem );
+    }
+    
+    inline void SemaphoreSignal( semaphoreid_t& semaphoreid, int32_t countWaiting )
+    {
+        while( countWaiting-- > 0 )
+		{
+			semaphore_signal( semaphoreid.sem );
+		}
+    }
+}
+
+#else // POSIX
+
+#include <semaphore.h>
+
+namespace enki
+{
+    
     struct semaphoreid_t
     {
         sem_t   sem;
     };
-    
-        
-    // declare the thread start function as:
-    // THREADFUNC_DECL MyThreadStart( void* pArg );
-    inline bool ThreadCreate( threadid_t* returnid, void* ( *StartFunc) (void* ), void* pArg )
-    {
-        // posix equiv pthread_create
-        int32_t retval = pthread_create( returnid, NULL, StartFunc, pArg );
-
-        return  retval == 0;
-    }
-    
-    inline bool ThreadTerminate( threadid_t threadid )
-    {
-        // posix equiv pthread_cancel
-        return pthread_cancel( threadid ) == 0;
-    }
-    
-    inline uint32_t GetNumHardwareThreads()
-    {
-        return (uint32_t)sysconf( _SC_NPROCESSORS_ONLN );
-    }
-    
-    inline void SemaphoreCreate( semaphoreid_t& semaphoreid )
+	
+	inline void SemaphoreCreate( semaphoreid_t& semaphoreid )
     {
 		int err = sem_init( &semaphoreid.sem, 0, 0 );
 		assert( err == 0 );
@@ -151,6 +205,6 @@ namespace enki
 		}
     }
 }
+#endif
 
-#endif // posix
 

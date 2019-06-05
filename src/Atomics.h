28b5a002495b7968216750a8e7877f213b3aafd9
Doug Binks <doug@enkisoftware.com>
2019-06-05 14:04:58 +0100

    Formatting changes.

--- src/Atomics.h
+++ src/Atomics.h
@@ -49,11 +49,11 @@ namespace enki
     // returns old *pDest (so if successfull, returns compareWith)
     inline uint32_t AtomicCompareAndSwap( volatile uint32_t* pDest, uint32_t swapTo, uint32_t compareWith )
     {
-       #ifdef _WIN32
-            return _InterlockedCompareExchange( (volatile long*)pDest,swapTo, compareWith );
-        #else
-            return __sync_val_compare_and_swap( pDest, compareWith, swapTo );
-        #endif      
+#ifdef _WIN32
+		return _InterlockedCompareExchange( (volatile long*)pDest,swapTo, compareWith );
+#else
+		return __sync_val_compare_and_swap( pDest, compareWith, swapTo );
+#endif      
     }
 
     inline int32_t AtomicCompareAndSwap( volatile int32_t* pDest, int32_t swapTo, int32_t compareWith )
@@ -67,11 +67,11 @@ namespace enki
 
     inline uint64_t AtomicCompareAndSwap( volatile uint64_t* pDest, uint64_t swapTo, uint64_t compareWith )
     {
-       #ifdef _WIN32
-            return _InterlockedCompareExchange64( (__int64 volatile*)pDest, swapTo, compareWith );
-        #else
-            return __sync_val_compare_and_swap( pDest, compareWith, swapTo );
-        #endif      
+#ifdef _WIN32
+		return _InterlockedCompareExchange64( (__int64 volatile*)pDest, swapTo, compareWith );
+#else
+		return __sync_val_compare_and_swap( pDest, compareWith, swapTo );
+#endif      
     }
 
     inline void* AtomicCompareAndSwapPtr( void* volatile* pDest, void* swapTo, void* compareWith )
@@ -96,11 +96,11 @@ namespace enki
     // Atomically performs: tmp = *pDest; *pDest += value; return tmp;
     inline int32_t AtomicAdd( volatile int32_t* pDest, int32_t value )
     {
-       #ifdef _WIN32
-            return _InterlockedExchangeAdd( (long*)pDest, value );
-        #else
-            return __sync_fetch_and_add( pDest, value );
-        #endif      
+#ifdef _WIN32
+		return _InterlockedExchangeAdd( (long*)pDest, value );
+#else
+		return __sync_fetch_and_add( pDest, value );
+#endif      
     }
 
 }

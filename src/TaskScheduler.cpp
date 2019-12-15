20e8b4edbc15c1c2406ba87fb4ccb9e977f19561
Doug Binks <doug@enkisoftware.com>
2019-12-15 14:14:46 +0000

    Compile fix for when NOMINMAX globally defined.

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -880,7 +880,9 @@ void TaskScheduler::Initialize()
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <Windows.h>
 
 namespace enki

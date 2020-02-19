e04b7e4c9c7f113f990a4622b3327aab15150d39
Doug Binks <doug@enkisoftware.com>
2020-02-19 16:24:40 +0000

    Removed Win32 workaround for VS2013 as enkiTS requires full C++11 compliance.

--- example/Timer.h
+++ example/Timer.h
@@ -1,31 +1,20 @@
 #pragma once
 
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
-#else
-    #include <chrono>
-#endif
+#include <chrono>
 
 // simple time class you shouldn't use outside example code.
-// made to get around precision issue with chrono on windows with VS2013
-// time returned is in ms
 class Timer
 {
-    double timeMS;
-    bool   bRunning;
+    double timeMS = 0.0;
+    bool   bRunning = false;
 public:
-    Timer() : timeMS(0.0), bRunning(false) {}
+    Timer() = default;
 
     // start does not reset time to 0, use reset
     void Start()
     {
         bRunning = true;
-#ifdef _WIN32
-        QueryPerformanceCounter( &start );
-#else
         start = std::chrono::high_resolution_clock::now();
-#endif
     }
 
     double GetTimeMS()
@@ -34,19 +23,8 @@ public:
         {
             return timeMS;
         }
-#ifdef _WIN32
-        LARGE_INTEGER stop;
-        QueryPerformanceCounter( &stop );
-        LARGE_INTEGER elapsed;
-        elapsed.QuadPart = stop.QuadPart - start.QuadPart;
-
-        LARGE_INTEGER freq;
-        QueryPerformanceFrequency( &freq );
-        return (double)( 1000 * elapsed.QuadPart ) / (double)freq.QuadPart;
-#else
         auto stop = std::chrono::high_resolution_clock::now();
         return 1000.0 * std::chrono::duration_cast<std::chrono::duration<double>>(stop - start).count();
-#endif
     };
 
     void Stop()
@@ -65,9 +43,5 @@ public:
     }
 
 private:
-#ifdef _WIN32
-    LARGE_INTEGER start;
-#else
-    std::chrono::high_resolution_clock::time_point start;
-#endif
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 };

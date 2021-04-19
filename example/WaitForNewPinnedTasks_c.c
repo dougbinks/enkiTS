94d26ca2a17a2bd54e25c1757375a4423eb6d0e1
Doug Binks <doug@enkisoftware.com>
2021-04-19 16:56:06 +0100

    Switched WaitForNewPinnedTasks_c.c to use an enkiTS task thread for simplicity.

--- example/WaitForNewPinnedTasks_c.c
+++ example/WaitForNewPinnedTasks_c.c
@@ -22,55 +22,7 @@
 #include <string.h>
 #include <assert.h>
 
-// XPLATF Thread handling functions for C
-#ifdef _WIN32
-
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-    
-typedef HANDLE threadid_t;
-#define THREADFUNC_DECL DWORD WINAPI
-
-// declare the thread start function as:
-// THREADFUNC_DECL MyThreadStart( void* pArg );
-int32_t ThreadCreate( threadid_t* returnid, DWORD ( WINAPI *StartFunc) (void* ), void* pArg )
-{
-    DWORD threadid;
-    *returnid = CreateThread( 0, 0, StartFunc, pArg, 0, &threadid );
-    return  *returnid != NULL;
-}
-
-int32_t ThreadJoin( threadid_t threadid )
-{
-    return WaitForSingleObject( threadid, INFINITE ) == 0;
-}
-
-#else // posix
-#include <pthread.h>
-#include <unistd.h>
-
-typedef pthread_t threadid_t;  
-#define THREADFUNC_DECL void*
-
-// declare the thread start function as:
-// THREADFUNC_DECL MyThreadStart( void* pArg );
-int32_t ThreadCreate( threadid_t* returnid, void* ( *StartFunc) (void* ), void* pArg )
-{
-    int32_t retval = pthread_create( returnid, NULL, StartFunc, pArg );
-
-    return  retval == 0;
-}
-    
-int32_t ThreadJoin( threadid_t threadid )
-{
-    return pthread_join( threadid, NULL ) == 0;
-}
-#endif
-
 enkiTaskScheduler*    pETS;
-enkiPinnedTask*       pPinnedTaskPretendIO;
-enkiPinnedTask*       pPinnedTaskStop;
 
 
 void PinnedTaskPretendIOFunc( void* pArgs_ )
@@ -86,45 +38,42 @@ void PinnedTaskStopFunc( void* pArgs_ )
     g_Stop = 1; // call stop
 }
 
-THREADFUNC_DECL ThreadFunc( void* pArgs_ )
+void PinnedTaskRunPinnedTaskLoop( void* pArgs_ )
 {
-    uint32_t threadNum;
-    int retVal;
-
-    // we register to first address
-    retVal = enkiRegisterExternalTaskThreadNum( pETS, enkiGetNumFirstExternalTaskThread() );
-    assert( retVal );
-
-    threadNum = enkiGetThreadNum( pETS );
-    assert( threadNum == enkiGetNumFirstExternalTaskThread() );
-    printf("ThreadFunc running on thread %d (should be thread %d)\n", threadNum, enkiGetNumFirstExternalTaskThread() );
+    uint32_t threadNumDesired = *(uint32_t*)pArgs_;
+    uint32_t threadNum = enkiGetThreadNum( pETS );
+    assert( threadNum == threadNumDesired );
+    printf("PinnedTaskRunPinnedTaskLoop running on thread %d (should be thread %d)\n", threadNum, threadNumDesired );
 
     while( 0 == g_Stop )
     {
         enkiWaitForNewPinnedTasks( pETS );
         enkiRunPinnedTasks( pETS );
     }
-
-    enkiDeRegisterExternalTaskThread( pETS );
-    return 0;
 }
 
 int main(int argc, const char * argv[])
 {
     struct enkiTaskSchedulerConfig config;
+    enkiPinnedTask* pPinnedTaskRunPinnedTaskLoop;
+    enkiPinnedTask* pPinnedTaskPretendIO;
+    enkiPinnedTask* pPinnedTaskStop;
 
     pETS = enkiNewTaskScheduler();
 
     // get default config and request one external thread
     config = enkiGetTaskSchedulerConfig( pETS );
-    config.numExternalTaskThreads = 1;
+    config.numTaskThreadsToCreate += 1; // Create 1 extra thread for IO tasks (could create more if needed)
     enkiInitTaskSchedulerWithConfig( pETS, config );
 
-    threadid_t threadID;
-    ThreadCreate( &threadID, ThreadFunc, NULL );
+    uint32_t threadNumIOTasks = config.numTaskThreadsToCreate; // thread 0 is this thread, so last thread is num threads created.
+
+    // create task to run pinned task loop
+    pPinnedTaskRunPinnedTaskLoop = enkiCreatePinnedTask( pETS, PinnedTaskRunPinnedTaskLoop, threadNumIOTasks );
+    enkiAddPinnedTaskArgs( pETS, pPinnedTaskRunPinnedTaskLoop, &threadNumIOTasks );
 
     // send pretend IO commands to external thread
-    pPinnedTaskPretendIO = enkiCreatePinnedTask( pETS, PinnedTaskPretendIOFunc, enkiGetNumFirstExternalTaskThread() );
+    pPinnedTaskPretendIO = enkiCreatePinnedTask( pETS, PinnedTaskPretendIOFunc, threadNumIOTasks );
     for( int32_t i=0; i<5; ++i )
     {
         // we re-use one task here as we are waiting for each to complete
@@ -138,13 +87,14 @@ int main(int argc, const char * argv[])
     enkiDeletePinnedTask( pETS, pPinnedTaskPretendIO );
 
     // send stop to external thread
-    pPinnedTaskStop = enkiCreatePinnedTask( pETS, PinnedTaskStopFunc, enkiGetNumFirstExternalTaskThread() );
+    pPinnedTaskStop = enkiCreatePinnedTask( pETS, PinnedTaskStopFunc, threadNumIOTasks );
     enkiAddPinnedTask( pETS, pPinnedTaskStop );
 
+    // wait for task loop to complete
+    enkiWaitForPinnedTask( pETS, pPinnedTaskRunPinnedTaskLoop );
 
-    ThreadJoin( threadID );
     enkiDeletePinnedTask( pETS, pPinnedTaskStop );
-
+    enkiDeletePinnedTask( pETS, pPinnedTaskRunPinnedTaskLoop );
 
     enkiDeleteTaskScheduler( pETS );
     printf("WaitForNewPinnedTasks_c.c completed\n" );

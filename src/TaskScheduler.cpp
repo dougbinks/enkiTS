f54acf398aec4f3969ba508d1d7411b8555ad8df
Doug Binks <doug@enkisoftware.com>
2023-08-08 18:49:36 +0100

    Macro checks for WIN32_LEAN_AND_MEAN and NOMINMAX, fixes #100

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -48,8 +48,13 @@ namespace
 #endif
 
 #ifdef ENKI_USE_WINDOWS_PROCESSOR_API
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+    #define NOMINMAX
+#endif
 #include "Windows.h"
 #endif
 
